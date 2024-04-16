e0f1f5b1a4a67b9c9b30f398829f20999677081e
Wonjung <gimpo5975@naver.com>
2024-04-17 06:50:53 +0900

    개인 쇼핑몰 프로젝트생성 및 상품 entity작성

---
+++ src/main/java/com/kwj/repository/ItemRepository.java
@@ -0,0 +1,16 @@
+package com.kwj.repository;
+
+import com.kwj.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    List<Item> findByItemNm(String itemNm);
+
+    @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
+}//end of interface
